4adde66e86b9dc6190668c626da0075da06ff487
Batuhan <62402999+valco72@users.noreply.github.com>
2021-05-17 13:08:18 +0300

    Add files via upload

--- plugins/someonesay.js
+++ plugins/someonesay.js
@@ -120,11 +120,11 @@ else if (Config.WORKTYPE == 'public') {
 
     }));
 
-    Asena.addCommand({ pattern: 'tweet ?(.*)', fromMe: true, desc: "WhatsBAT sizin adiniza tweet atar", dontAddCommandList: true }, (async (message, match) => {
+    Asena.addCommand({ pattern: 'tweet ?(.*)', fromMe: true, desc: "WhatsGAIA sizin adiniza tweet atar", dontAddCommandList: true }, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
 
-        var ttinullimage = await axios.get(`https://nekobot.xyz/api/imagegen?type=tweet&username=WhatsBAT&text=${match[1].replace(/Ö/g, "%C3%96").replace(/ö/g, "%C3%B6").replace(/ü/g, "%C3%BC").replace(/Ü/g, "%C3%9C").replace(/Ğ/g, "%C4%9E").replace(/ğ/g, "%C4%9F").replace(/ş/g, "%C5%9F").replace(/Ş/g, "%C5%9E").replace(/ç/g, "%C3%A7").replace(/Ç/g, "%C3%87").replace(/ı/g, "%C4%B1").replace(/i/g, "%69").replace(/"/g, "%22").replace(/İ/g, "%C4%B0")}&raw=1`, { responseType: 'arraybuffer' })
+        var ttinullimage = await axios.get(`https://nekobot.xyz/api/imagegen?type=tweet&username=WhatsGAIA&text=${match[1].replace(/Ö/g, "%C3%96").replace(/ö/g, "%C3%B6").replace(/ü/g, "%C3%BC").replace(/Ü/g, "%C3%9C").replace(/Ğ/g, "%C4%9E").replace(/ğ/g, "%C4%9F").replace(/ş/g, "%C5%9F").replace(/Ş/g, "%C5%9E").replace(/ç/g, "%C3%A7").replace(/Ç/g, "%C3%87").replace(/ı/g, "%C4%B1").replace(/i/g, "%69").replace(/"/g, "%22").replace(/İ/g, "%C4%B0")}&raw=1`, { responseType: 'arraybuffer' })
 
         await message.client.sendMessage(message.jid, Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.png, caption: 'Made by WhatsGAIA' })
 
